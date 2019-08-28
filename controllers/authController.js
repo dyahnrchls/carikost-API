0b76b1b3a683e4a876c7a8db833ce6486578a454
dyah <dyahnrchls@gmail.com>
2019-08-28 14:10:19 +0700

    update database (change data type) and authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,19 +3,31 @@ const models = require('../models')
 const User = models.user
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
+const Joi = require('@hapi/joi')
 
 exports.register = (req, res) => {
+    const schema = {
+        username: Joi.string().alphanum().min(4).max(30).required(),
+        email: Joi.string().email({ minDomainSegments: 2 }).required(),
+        password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/).required()
+    }
+
     const { email, username } = req.body
     const data = req.body
+    const result = Joi.validate(req.body, schema)
 
-    User.findOne({ where: {[Op.or] : [{email}, {username}]}})
+    if (result.error) {
+        return res.send('error username/email/password')
+    }
+
+    User.findOne({ where: { [Op.or]: [{ email }, { username }] } })
         .then(user => {
-            if(user){
-                return res.status(409).send({message: 'email/username exist'})
-            }else{
+            if (user) {
+                return res.status(409).send({ message: 'email/username exist' })
+            } else {
                 return User.create(data)
                     .then(user => {
-                        const token = jwt.sign({ id: user.id }, 'my-secret-key', { expiresIn:  14400})
+                        const token = jwt.sign({ id: user.id }, 'my-secret-key', { expiresIn: 5 })
                         let { id, username, email } = user
                         res.send({
                             id,
@@ -31,13 +43,13 @@ exports.register = (req, res) => {
 
 
 exports.login = (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
 
-    User.findOne({ where: {email, password}})
+    User.findOne({ where: { email, password } })
         .then(user => {
-            if(user) {
-                const token = jwt.sign({ id: user.id }, 'my-secret-key', {expiresIn: 14400})
-                let {id, username, email} = user
+            if (user) {
+                const token = jwt.sign({ id: user.id }, 'my-secret-key', { expiresIn: 5 })
+                let { id, username, email } = user
                 res.send({
                     message: 'You are logged in!',
                     id,
@@ -53,5 +65,3 @@ exports.login = (req, res) => {
         })
         .catch(err => res.status(400).send(err))
 }
-
-
---
+++ migrations/20190828070819-change-longitude-latitude.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn('dorms',
+      'latitude',
+      {
+        type: Sequelize.DOUBLE
+      },
+      'longitude',
+      {
+        type: Sequelize.DOUBLE
+      }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
